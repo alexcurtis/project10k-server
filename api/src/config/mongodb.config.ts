136f7e51bbf8850caf233047d0c4fce82fd81b6c
Alexander Curtis <alexander@Alexanders-MacBook-Pro.local>
2024-08-24 23:32:11 +0100

    Dockerify Ussec Microservice

--- api/src/config/mongodb.config.ts
+++ api/src/config/mongodb.config.ts
@@ -5,9 +5,6 @@ import { registerAs } from "@nestjs/config";
  */
 export default registerAs("mongodb", () => {
     const { MONGO_URI } = process.env;
-
-    console.log("MONGO URI", MONGO_URI);
-
     return {
         uri: `${MONGO_URI}`,
     };
